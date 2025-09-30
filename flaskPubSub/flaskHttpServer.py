cc952f7133e6edbd9d54619b5f2a3da358d4cc78
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-09-30 12:03:38 -0400

    update

---
+++ flaskPubSub/flaskHttpServer.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+
+
+# Define the Flask app
+app = Flask(__name__)
+
+subscribers = {}
+
+def home():
+  return "Hello from Flask!"
+
+@app.route('/', methods=['GET'])
+def root():
+  print(f"Hello at the root")
+  return jsonify({'main endpoint':'Ack'})
+
+@app.route('/list-subscribers', methods=['GET'])
+def listSubscribers():
+  return jsonify(subscribers)
+
+# Windows> curl.exe -X POST -H "Content-Type: application/json" -d "{\"name\":\"Alice\",\"URI\":\"http://good.site.com\"}" http://localhost:5000/submit
+
+@app.route('/add-subscriber', methods=['POST'])
+def addSubscriber():
+  data = request.json
+  name = data.get('name')
+  URI = data.get('URI')
+  subscribers[data] = URI
+  print(f"You entered: Name={name}, Address={URI}")
+  return jsonify({'message': f'You sent name: {name} and address: {URI}'})
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000, debug=True)
